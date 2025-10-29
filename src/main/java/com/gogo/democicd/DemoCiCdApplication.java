c3b5dcefcf0020966793e7a1a73cccfff0bc7db8
NdiayeGorgui <90470613+NdiayeGorgui@users.noreply.github.com>
2025-10-29 11:15:37 -0400

    war file

--- src/main/java/com/gogo/democicd/DemoCiCdApplication.java
+++ src/main/java/com/gogo/democicd/DemoCiCdApplication.java
@@ -2,12 +2,13 @@ package com.gogo.democicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class DemoCiCdApplication {
+public class DemoCiCdApplication extends SpringBootServletInitializer {
     @GetMapping
     public String getMessage() {
         return "Docker deploy to DockerHub and Tomcat";
